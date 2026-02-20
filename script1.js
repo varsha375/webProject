6e1416a1c264c0393fa27b617bb0d99b65cb609b
varsha375 <varshabaitmangalkar22@gmail.com>
2026-02-20 11:56:43 +0530

    welcome to this project

--- script1.js
+++ script1.js
@@ -1,11 +1,8 @@
 function square() {
   var a = document.getElementById("value1").value;
+  document.write(a * a);
+}
+function cube() {
+  var a = document.getElementById("value2").value;
   document.write(a * a * a);
 }
-
-
-// square
-// function square() {
-//   var a = document.getElementById("value1").value;
-//   document.write(a * a);
-// }
