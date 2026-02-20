3ff9c7a6a954386c21ee5949c6f874f0c2cf358b
varsha375 <varshabaitmangalkar22@gmail.com>
2026-02-20 11:35:18 +0530

    new

---
+++ script.js
@@ -0,0 +1,78 @@
+// var rabbitName = "archimedes",
+//   age = 3;
+// if (rabbitName == "archimedes" && age == 1) {
+//   alert("hello archimedes,welcome home");
+// }
+// if (age == 0 || age == 1) {
+//   alert("hello i am your junior");
+// }
+// var isYoung = age < 5;
+// if (!isYoung) {
+//   alert("rabbit is old");
+// } else {
+//   alert("rabbit is young");
+// }
+
+// for loop
+// for (var i = 0; i < 10; i++) {
+//   document.write("this is row" + i + "<br/>");
+// }
+
+// while loop
+// var count = 0;
+// while (count < 10) {
+//   document.write("count" + count + "<br/>");
+// }
+// var num1 = 100;
+// var num2 = 200;
+// if (num1 > num2) {
+//   alert("num1 is bigger");
+// }
+// var num2bigger = num1 > num2;
+// if (num2bigger) {
+//   alert("num2 is bigger ");
+// }
+// if (num1 == num2) {
+//   alert("num1 is equal");
+// }
+// if (num1 != num2) {
+//   alert("num1 is not equal to num2");
+
+// var currentTime = 8;
+// if (currentTime > 5 && currentTime < 12) {
+//   alert("Good morning!");
+// } else if (currentTime > 12 && currentTime < 18) {
+//   alert("Good afternoon!");
+// } else {
+//   alert("Lunch is over");
+// }
+
+// Before    // alert("Take care");
+
+// var count = 0;
+// while (count < 10) {
+//   document.write("count" + count + "<br/>");
+//   count++;
+// }
+
+// function clickMe() {
+//   alert("clicked 1");
+// }
+// clickMe();
+
+// var clickMe = function () {
+//   alert("clicked 2");
+// };
+// clickMe();
+
+function sum(x, y) {
+  return x * y;
+}
+var six = sum(2, 3);
+alert(sum(5, 10));
+document.write(sum(5, sum(5, 5)));
+
+// function sum(x, y) {
+//   return x * y;
+// }
+// console.log(sum(12, 20));
---
+++ script1.js
@@ -0,0 +1,11 @@
+function square() {
+  var a = document.getElementById("value1").value;
+  document.write(a * a * a);
+}
+
+
+// square
+// function square() {
+//   var a = document.getElementById("value1").value;
+//   document.write(a * a);
+// }
